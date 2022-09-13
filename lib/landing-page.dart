013904aa0f30365cd1151e29ad21c545a105d99e
paradox-shivay <shivay.paradox@gmail.com>
2022-09-13 14:25:00 +0530

    setup

---
+++ lib/landing-page.dart
@@ -0,0 +1,11 @@
+import 'package:basic_intro/src/modules/sign-in.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignIn();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:basic_intro/landing-page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Into',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingPage(),
+    );
+  }
+}
